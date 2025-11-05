339463bbf6886a281417d333d69bfbf0097c8bf4
Nejmeddin Ben Maatoug <nejmeddin.benmaatoug@enis.tn>
2025-11-05 09:55:07 +0100

    Ajout de message bienvenue

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 2, b = 3;
+        System.out.println("Bienvenue");
         Operation op = new Operation();
         System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
         System.out.println(a + " - " + b + " = " + Operation.soustraction(a, b));
