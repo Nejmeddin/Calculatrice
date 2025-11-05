c58c74dd903210c022b160b44b22bc32ad1c1336
Nejmeddin Ben Maatoug <nejmeddin.benmaatoug@enis.tn>
2025-11-05 09:01:15 +0100

    Creation d'un fichier .gitignore pour ignorer les fichiers .class

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        Operation op = new Operation();
+        int res = op.multiplication(a, b);
+        System.out.println("a * b = " + res);
+    }
+}
--- Operation.java
+++ Operation.java
@@ -1,5 +1,6 @@
 public class Operation {
     public static int multiplication(int x, int y) {
+        System.out.println("Bonjour");
         return x * y;
     }
 }
