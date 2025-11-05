daad3cb301aa0363f720953eb97d09952de76443
Nejmeddin Ben Maatoug <nejmeddin.benmaatoug@enis.tn>
2025-11-05 09:06:04 +0100

    ajouterles foction soustr & add à Operation.java

--- Operation.java
+++ Operation.java
@@ -2,5 +2,10 @@ public class Operation {
     public static int multiplication(int x, int y) {
         System.out.println("Bonjour");
         return x * y;
+    public static int soustraction(int a, int b) { return a - b; }
+    public static int addition(int a, int b) { return a + b; }
+
+    public static double division(int a, int b) {
+        return (double) a / b;
     }
 }
