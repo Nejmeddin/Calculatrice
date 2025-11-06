25e01a97f2616b6dd8f9e65d9fb8b1a69a91042f
Nejmeddin Ben Maatoug <nejmeddin.benmaatoug@enis.tn>
2025-11-06 16:18:37 +0100

    correction de confusion

--- Main.java
+++ Main.java
@@ -3,16 +3,11 @@ public class Main {
         int a = 2, b = 0;
         System.out.println("Bienvenue");
         Operation op = new Operation();
-<<<<<<< HEAD
         System.out.println(a + " * " + b + " = " + Operation.multiplication(a, b));
         System.out.println(a + " - " + b + " = " + Operation.soustraction(a, b));
         System.out.println(a + " + " + b + " = " + Operation.addition(a, b));
-        System.out.println("Aurevoir");
-=======
-        System.out.println(a + " * " + b + " = " + op.multiplication(a, b));
-        System.out.println(a + " - " + b + " = " + op.soustraction(a, b));
-        System.out.println(a + " + " + b + " = " + op.addition(a, b));
         System.out.println(a+" / "+b+" = "+ op.division(a,b));
->>>>>>> division
+        System.out.println("Aurevoir");
     }
 }
+
